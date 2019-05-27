ff5c28efc07a49473ca76768353a9e8992568253
rohit <ganguru.rohith@gmail.comclea>
2019-05-27 14:28:44 +0530

    Task 1 Annotations completed

---
+++ src/main/java/com/stackroute/Main.java
@@ -0,0 +1,23 @@
+package com.stackroute;
+
+import com.stackroute.config.AppConfig;
+import com.stackroute.domain.Movie;
+import com.stackroute.domain.Actor;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main
+{
+    public static void main(String[] args )
+    {
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+
+        Movie movie = (Movie) ctx.getBean("movie");
+        System.out.println(movie);
+
+        Movie movie1 = (Movie) ctx.getBean("movie1");
+        System.out.println(movie1);
+    }
+}
---
+++ src/main/java/com/stackroute/config/SpringBeanConfiguration.java
@@ -0,0 +1,28 @@
+package com.stackroute.config;
+
+import com.stackroute.domain.Actor;
+import com.stackroute.domain.Movie;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringBeanConfiguration {
+
+    public Actor getActor(){
+        return new Actor("rohith","male",22);
+    }
+
+    public Actor getActor1(){
+        return new Actor("vijay","male",23);
+    }
+    @Bean(name="movie")
+    public Movie getMovie(){
+        return new Movie(1,"temper",getActor());
+
+    }
+
+    @Bean(name="movie1")
+    public Movie getMovie1(){
+        return new Movie(2,"rabhasa",getActor1());
+    }
+}
